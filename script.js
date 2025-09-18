434d03833c7b45af1d9c24e9477d125cb9f799bc
Max Sanchez <145044997+MaxSanchez99@users.noreply.github.com>
2025-09-17 23:11:49 -0500

    Added functionality to calculator
    All buttons where given event listeners depending
    on if they were numbers or operators. Display is
    updated when users inputs numbers or performs
    operations.

--- script.js
+++ script.js
@@ -30,3 +30,72 @@ function operate(operator, num1, num2) {
     }
 }
 
+// get first number
+// get operator
+// get second number
+// computer result after an = button is clicked
+// result gets stored into first number 
+
+let firstNum = '';
+let secondNum = '';
+let operator = '';
+let isResult = false;
+
+let numbers = document.querySelectorAll(".num-btn")
+let operators = document.querySelectorAll(".op-btn")
+let display = document.querySelector('.display')
+
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {
+
+        if (operator === '' && !isResult) {
+            firstNum += number.textContent;
+            display.textContent = firstNum;
+            console.log(firstNum);
+        }
+        else if (operator === '' && isResult) {
+            firstNum = number.textContent;
+            display.textContent = firstNum
+            isResult = false;
+            console.log(firstNum);
+        }
+        else {
+            secondNum += number.textContent;
+            display.textContent = secondNum;
+            console.log(secondNum);
+        }
+    });
+});
+
+operators.forEach((op) => {
+    op.addEventListener('click', () => {
+        if (operator !== '' && secondNum !== '') {
+            let output = operate(operator, +firstNum, +secondNum);
+            console.log(output);
+            display.textContent = output
+
+            firstNum = output.toString();
+            secondNum = '';
+            isResult = true;
+
+            if (op.textContent === '=') { operator = ''; }
+
+            else { operator = op.textContent };
+        }
+
+        else {
+            if (op.textContent !== '=') {
+                operator = op.textContent;
+            }
+        }
+    });
+});
+
+let clearBtn = document.querySelector('#clear-btn');
+clearBtn.addEventListener('click', () => {
+    firstNum = '';
+    secondNum = '';
+    operator = '';
+    display.textContent = '0';
+
+})
